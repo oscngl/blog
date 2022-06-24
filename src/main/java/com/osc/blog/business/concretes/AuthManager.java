ea76cac397ee92db94562e52ee577e6603dbfe6d
Ömer Said ÇENGEL <oscngl@gmail.com>
2022-06-24 16:51:16 +0300

    PasswordEncoder implemented.

--- src/main/java/com/osc/blog/business/concretes/AuthManager.java
+++ src/main/java/com/osc/blog/business/concretes/AuthManager.java
@@ -14,6 +14,7 @@ import com.osc.blog.entities.concretes.Role;
 import com.osc.blog.entities.concretes.User;
 import com.osc.blog.entities.dtos.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,6 +27,7 @@ public class AuthManager implements AuthService {
     private final UserService userService;
     private final RoleService roleService;
     private final EmailSenderService emailSenderService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final ConfirmationTokenService confirmationTokenService;
 
     @Override
@@ -34,6 +36,7 @@ public class AuthManager implements AuthService {
         if(!role.isSuccess()) {
             return new ErrorResult(role.getMessage());
         }
+        userDto.setPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
         DataResult<User> user = userService.save(userDto, role.getData());
         if(!user.isSuccess()) {
             return new ErrorResult(user.getMessage());
--- src/main/java/com/osc/blog/security/config/SecurityConfig.java
+++ src/main/java/com/osc/blog/security/config/SecurityConfig.java
@@ -1,13 +1,23 @@
 package com.osc.blog.security.config;
 
+import com.osc.blog.business.abstracts.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -16,7 +26,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .disable()
                 .httpBasic()
                 .disable();
+    }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(userService);
+        return provider;
     }
 
 }
